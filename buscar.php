5afbea06c479d8967f5efe882242231dc991a50d
Bruno Holanda <br.holanda15@gmail.com>
2020-05-17 20:33:50 -0300

    Capturando cursos na alura

--- buscar.php
+++ buscar.php
@@ -1,8 +1,19 @@
 <?php 
 
-use GuzzleHttp\Client;
+require 'vendor/autoload.php';
 
-$client = new Client();
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\CssSelector\CssSelectorConverter;
+
+$client = new Client(['verify' => false]);
 $response = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
 
 $html = $response->getBody();
+$crawler = new Crawler($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach ($cursos as $curso ) {
+    echo $curso->textContent . PHP_EOL;
+}
