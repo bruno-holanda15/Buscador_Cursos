8490ae1d9d07726ad50561776c1ae68d01b7dd2e
Bruno Holanda <br.holanda15@gmail.com>
2020-05-19 11:53:03 -0300

    Adicionado codesniffer --dev, ajustando Buscador conforme PSR12

--- src/Buscador.php
+++ src/Buscador.php
@@ -5,32 +5,28 @@ namespace Alura\BuscadorDeCursos;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-class Buscador{
+class Buscador
+{
 
     private $httpClient;
     private $crawler;
 
-    public function __construct( Client $httpClient, Crawler $crawler){
-
+    public function __construct(Client $httpClient, Crawler $crawler)
+    {
         $this->httpClient = $httpClient;
         $this->crawler = $crawler;
-
     }
 
-    public function buscar( $url ){
-        
+    public function buscar($url)
+    {
         $response = $this->httpClient->request('GET', $url);
-        $html = $response->getBody();   
-
+        $html = $response->getBody();
         $this->crawler->addHtmlContent(($html));
         $elementosCursos = $this->crawler->filter('span.card-curso__nome');
         $cursos = [];
-        foreach ($elementosCursos as $elemento ) {
-
+        foreach ($elementosCursos as $elemento) {
             $cursos[] = $elemento->textContent;
-        
         }
         return $cursos;
     }
-    
 }
